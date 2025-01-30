319a27ff7d6c3a011dfa444331f7f7b01bbc6674
Henri Liiv <liiv.henri@gmail.com>
2025-01-30 21:38:51 +0200

    Add roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.create(email: "admin@simple_cmdb.com", password: "admin")
+user.add_role(:admin)  # Assign admin role
+
+viewer = User.create(email: "viewer@simple_cmdb.com", password: "viewer")
+viewer.add_role(:viewer)  # Assign viewer role
