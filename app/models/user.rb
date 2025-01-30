c1a79804cd5a029b2ece917ea5cfa4eefecaf99a
Henri Liiv <liiv.henri@gmail.com>
2025-01-30 21:16:21 +0200

    Modify the User Model
    Adding admin and viewer methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def admin?
+    has_role?(:admin)
+  end
+
+  def viewer?
+    has_role?(:viewer)
+  end
 end
